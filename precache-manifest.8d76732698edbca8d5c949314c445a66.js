c7904d67fddc4b111088ecbb686493200c04cd30
Sonish Maharjan <sonishmaharjan1@gmail.com>
2020-10-10 23:29:49 +0545

    Updates

---
+++ precache-manifest.8d76732698edbca8d5c949314c445a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae730a8bfed14c80be3d69366f18f81",
+    "url": "/todoapp-hooks-context/index.html"
+  },
+  {
+    "revision": "aa0bc06c539295c234fc",
+    "url": "/todoapp-hooks-context/static/css/main.58ef465c.chunk.css"
+  },
+  {
+    "revision": "2f6cb5e06adce9be6432",
+    "url": "/todoapp-hooks-context/static/js/2.ab847861.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoapp-hooks-context/static/js/2.ab847861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0bc06c539295c234fc",
+    "url": "/todoapp-hooks-context/static/js/main.eec8ed2c.chunk.js"
+  },
+  {
+    "revision": "810ca4b0a95346140d6f",
+    "url": "/todoapp-hooks-context/static/js/runtime-main.12e73098.js"
+  }
+]);
